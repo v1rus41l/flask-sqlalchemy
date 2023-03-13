ff8e6aff0cf45a37b215449c29cac1d047aad728
v1rus41l <romanmityakin2006@mail.ru>
2023-03-13 18:06:43 +0300

    abc

---
+++ data/users_resources.py
@@ -0,0 +1,49 @@
+from flask import jsonify
+from flask_restful import Resource, reqparse, abort
+
+from data import db_session
+from data.users import User
+from data.reqparse import parser
+
+
+def abort_if_user_not_found(user_id):
+    session = db_session.create_session()
+    user = session.query(User).get(user_id)
+    if not user:
+        abort(404, message=f"User {user_id} not found")
+
+class UsersResource(Resource):
+    def get(self, user_id):
+        abort_if_user_not_found(user_id)
+        session = db_session.create_session()
+        user = session.query(User).get(user_id)
+        return jsonify({'user': user.to_dict(
+            only=('name', 'about', 'email'))})
+
+    def delete(self, user_id):
+        abort_if_user_not_found(user_id)
+        session = db_session.create_session()
+        user = session.query(User).get(user_id)
+        session.delete(user)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class UsersListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        user = session.query(User).all()
+        return jsonify({'user': [item.to_dict(
+            only=('name', 'about', 'email')) for item in user]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        user = User(
+            name=args['name'],
+            about=args['about'],
+            email=args['email']
+        )
+        session.add(user)
+        session.commit()
+        return jsonify({'success': 'OK'})
--- test.py
+++ test.py
@@ -1,10 +1,31 @@
-from requests import get
+from requests import get, post, delete
 
-print(get('http://localhost:5000/api/news').json())
+# print(get('http://localhost:5000/api/v2/users').json())
+#
+# print(get('http://localhost:5000/api/v2/users/1').json())
+#
+# print(get('http://localhost:5000/api/v2/users/999').json())
+# # при условии, что пользователя с id = 999 нет в базе
+#
+# print(get('http://localhost:5000/api/v2/users/q').json())
 
-print(get('http://localhost:5000/api/news/1').json())
+# корректный запрос
+print(post('http://localhost:5000/api/v2/users',
+           json={'name': 'Scott',
+                 'about': 'want to be a programmer',
+                 'email': 'test@mail.ru'}).json())
 
-print(get('http://localhost:5000/api/news/999').json())
-# новости с id = 999 нет в базе
+# # некорректный: пустой запрос
+# print(post('http://localhost:5000/api/v2/users').json())
 
-print(get('http://localhost:5000/api/news/q').json())
+# # некорректный: передан только тимлид
+# print(post('http://localhost:5000/api/v2/users',
+#            json={'team_leader': 'Scott Jerard'}).json())
+#
+# # некорректный: переданы лишние данные
+# print(post('http://localhost:5000/api/v2/users',
+#            json={'title': 'Mission 1',
+#                  'team_leader': 'Scott Jerard',
+#                  'job': 'deployment of residential modules 1 and 2',
+#                  'collaborators': '1, 2',
+#                  'work_size': 5}).json())
