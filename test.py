70a4e9599a4faded93271210b652e69c9eafa656
v1rus41l <romanmityakin2006@mail.ru>
2023-03-16 15:12:21 +0300

    REST-API v1.0

--- test.py
+++ test.py
@@ -1,31 +1,37 @@
+import datetime
+
 from requests import get, post, delete
 
-# print(get('http://localhost:5000/api/v2/users').json())
-#
-# print(get('http://localhost:5000/api/v2/users/1').json())
-#
-# print(get('http://localhost:5000/api/v2/users/999').json())
-# # при условии, что пользователя с id = 999 нет в базе
-#
-# print(get('http://localhost:5000/api/v2/users/q').json())
+print(get('http://localhost:5000/api/v2/users').json())
+
+print(get('http://localhost:5000/api/v2/users/1').json())
+
+print(get('http://localhost:5000/api/v2/users/999').json())
+# при условии, что пользователя с id = 999 нет в базе
+
+print(get('http://localhost:5000/api/v2/users/q').json())
 
 # корректный запрос
 print(post('http://localhost:5000/api/v2/users',
            json={'name': 'Scott',
                  'about': 'want to be a programmer',
-                 'email': 'test@mail.ru'}).json())
+                 'email': 'test23@mail.ru'}).json())
 
-# # некорректный: пустой запрос
-# print(post('http://localhost:5000/api/v2/users').json())
+# некорректный: пустой запрос
+print(post('http://localhost:5000/api/v2/users').json())
 
-# # некорректный: передан только тимлид
-# print(post('http://localhost:5000/api/v2/users',
-#            json={'team_leader': 'Scott Jerard'}).json())
-#
-# # некорректный: переданы лишние данные
-# print(post('http://localhost:5000/api/v2/users',
-#            json={'title': 'Mission 1',
-#                  'team_leader': 'Scott Jerard',
-#                  'job': 'deployment of residential modules 1 and 2',
-#                  'collaborators': '1, 2',
-#                  'work_size': 5}).json())
+# некорректный: передано только имя
+print(post('http://localhost:5000/api/v2/users',
+           json={'name': 'Scott Jerard'}).json())
+
+# корректный запрос
+print(delete('http://localhost:5000/api/v2/users/3').json())
+
+# некорректный: новости с id = 999 нет в базе
+print(delete('http://localhost:5000/api/v2/users/999').json())
+
+# некорректный: неверный формат id
+print(delete('http://localhost:5000/api/v2/users/q').json())
+
+# некорректный: пустой запрос
+print(delete('http://localhost:5000/api/v2/users/users/q').json())
